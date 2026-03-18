2c421cd082cb2789714db18940854618bbc559d0
Kanak <goyalkanak0107@gmail.com>
2026-03-18 23:18:11 +0530

    added card

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -7,16 +7,62 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(""),
+        title: const Text("Let me track your balance"),
         centerTitle: true,
       ),
       body: Container(
+        width: double.infinity,
+        height: double.infinity,
         decoration: const BoxDecoration(
           image: DecorationImage(
               image: AssetImage("assets/images/img.png"),
               fit: BoxFit.cover,
           ),
         ),
+            child:Align(
+              alignment: Alignment.topCenter,
+            child: Card(
+              margin: EdgeInsets.all(20),
+              elevation: 8,
+              color: Colors.white,
+              child: Padding(
+                padding: EdgeInsets.all(25),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      "Today's Expenses",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+
+                    SizedBox(height: 20),
+
+                    ListTile(
+                      leading: Icon(Icons.fastfood),
+                      title: Text("Food"),
+                      trailing: Text("₹200"),
+                    ),
+
+                    ListTile(
+                      leading: Icon(Icons.shopping_bag),
+                      title: Text("Shopping"),
+                      trailing: Text("₹500"),
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.shopping_bag),
+                      title: Text("Movie"),
+                      trailing: Text("₹300"),
+                    ),
+
+                  ],
+                ),
+              ),
+            ),
+            ),
+
       ),
     );
   }
